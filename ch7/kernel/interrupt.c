e8e901c059bd6a449113c26fd4fdc187ae915fe9
ZHJD <2262040671@qq.com>
2020-01-31 23:45:41 +0800

    pic_init()

--- ch7/kernel/interrupt.c
+++ ch7/kernel/interrupt.c
@@ -1,11 +1,19 @@
 // 有关中断的内容在这个文件实现
-
 #include "interrupt.h"
 #include "stdint.h"
 #include "global.h"
+#include "io.h"
 
 #define IDT_DESC_CNT 0x21	// 目前总共支持的中断数,共33个中断处理程序
 
+//---------------------------------------------------------------------------------
+#define PIC_M_CTRL  0x20  // 主片的控制端口是0x20
+#define PIC_M_DATA  0x21  // 主片的数据端口是0x21
+#define PIC_S_CTRL  0xa0  // 从片的控制端口是0xa0
+#define PIC_S_DATA  0xa1  // 从片的数据端口是0xa1
+
+
+//--------------------------------------------------------------------------------
 // 中断门描述符结构体,按照中断描述符格式定义
 struct gate_desc
 {
@@ -17,10 +25,15 @@ struct gate_desc
 	uint16_t	func_offset_high_word;    // 中断处理程序在目标段内的偏移量31-16位
 };
 
+//---------------------------------------------------------------------------------
 // 使用static关键字， 该函数名和变量名在其它文件中不可见
 static void make_idt_desc(struct gate_desc* p_gdesc, uint8_t attr, intr_handler function);
+
 // 初始化中断描述符表
 static void idt_desc_init(void);
+
+// 初始化可编程中断控制器8259A
+static void pic_init(void);
 
 static struct gate_desc idt[IDT_DESC_CNT]; // idt是中断描述符表，定义了一个中断描述符表数组
 
@@ -47,6 +60,30 @@ static void idt_desc_init(void)
 	}
 	put_str(" idt_desc_init done\n");
 }
+
+static void pic_init(void)
+{
+  // 初始化主片
+  outb(PIC_M_CTRL, 0x11);
+  outb(PIC_M_DATA, 0x20);
+
+  outb(PIC_M_DATA, 0x04);
+  outb(PIC_M_DATA, 0x01);
+
+  // 初始化从片
+  outb(PIC_S_CTRL, 0x11);
+  outb(PIC_S_DATA, 0x28);
+
+  outb(PIC_S_DATA, 0x02);
+  outb(PIC_S_DATA, 0x01);
+
+  // 打开主片上的IR0，目前只接受时钟产生的中断
+  outb(PIC_M_DATA, 0xfe);
+  outb(PIC_S_DATA, 0xff);
+
+  put_str(" pic_init done\n");
+}
+
 
 // 完成有关中断的所有初始化工作
 void idt_init(void)
