df3e40094f5e58fa809835455b1e26e67b26a9f5
ZHJD <2262040671@qq.com>
2020-02-01 10:25:00 +0800

    'o'改正为0

--- ch7/kernel/tool/generate_32_intr_call.py
+++ ch7/kernel/tool/generate_32_intr_call.py
@@ -1,17 +1,18 @@
 # python3 program
 def gen(i):
-	s = 'VECTOR'
-	num = str(hex(int(i)))
-	if len(num) == 3:
-		num = num[:2] + '0' + num[2:]
-	s += ' '
-	s += num
-	s += ' '
-	if num == '0x1e':
-		s += 'ERROR_CODE'
-	else:
-		s += 'ZERO'
-	return s
+    s = 'VECTOR'
+    num = str(hex(int(i)))
+    num = '0' + num[1:]
+    if len(num) == 3:
+        num = num[:2] + '0' + num[2:]
+    s += ' '
+    s += num
+    s += ', '
+    if num == '0x1e':
+        s += 'ERROR_CODE'
+    else:
+         s += 'ZERO'
+    return s
 
 for i in range(0, 33):
-	print(gen(i))
+    print(gen(i))
