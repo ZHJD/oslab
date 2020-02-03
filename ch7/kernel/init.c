94f2009b8c1645c8eaa02cb8262300267872d674
ZHJD <2262040671@qq.com>
2020-02-03 21:14:42 +0800

    调用timer_init()

--- ch7/kernel/init.c
+++ ch7/kernel/init.c
@@ -1,10 +1,13 @@
 #include "init.h"
 #include "print.h"
 #include "interrupt.h"
-
+#include "timer.h"
 // 负责初始化所有模块
 void init_all(void)
 {
-  put_str("init_all\n");
-  idt_init();  // 初始化中断
+    put_str("init_all\n");
+
+    timer_init(); // 初始化pit8253
+
+     idt_init();  // 初始化中断
 }
