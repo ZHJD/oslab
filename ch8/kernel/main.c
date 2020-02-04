3088b99797dff93f178fa202df8006c3de4e9bbc
ZHJD <2262040671@qq.com>
2020-02-04 13:50:04 +0800

    添加测试ASSERT的语句

--- ch8/kernel/main.c
+++ ch8/kernel/main.c
@@ -1,9 +1,11 @@
 #include "print.h"
 #include "init.h"
+#include "debug.h"
 int main(void) {
 	put_str("I am a kernel.\n");
 	init_all();
-  asm volatile("sti"); // 开中断
+    ASSERT(1 == 2);
+    asm volatile("sti"); // 开中断
 	while(1);
 	return 0;
 }
