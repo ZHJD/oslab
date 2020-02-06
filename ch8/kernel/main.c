bea1d82bc121c1b25edb44ce7f4eec050f39888d
ZHJD <2262040671@qq.com>
2020-02-06 23:19:35 +0800

    去掉\n

--- ch8/kernel/main.c
+++ ch8/kernel/main.c
@@ -1,12 +1,13 @@
 #include "print.h"
 #include "init.h"
-#include "debug.h"
+#include "memory.h"
+
 int main(void) {
 	put_str("I am a kernel.\n");
 	init_all();
    
     void* vaddr = get_kernel_pages(4);
-    put_str("get kernel pages start virtual address:0x\n");
+    put_str("get kernel pages start virtual address:0x");
     put_int((uint32_t)vaddr);
     put_char('\n');
    // asm volatile("sti"); // 开中断
