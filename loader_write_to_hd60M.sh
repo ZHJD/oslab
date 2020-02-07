13a606bfa7bfd82719fab0b65842ba022338b0c2
ZHJD <2262040671@qq.com>
2020-02-07 13:34:12 +0800

    修改写硬盘脚本

--- loader_write_to_hd60M.sh
+++ loader_write_to_hd60M.sh
@@ -1,2 +1,2 @@
 # 把loader.bin 写入硬盘映像
-dd if=./ch5/boot/loader.bin of=./hd60M.img bs=512 count=4 seek=1 conv=notrunc       
+dd if=./ch8/boot/loader.bin of=./hd60M.img bs=512 count=4 seek=1 conv=notrunc       
