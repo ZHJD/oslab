df20cdb5f26380a46811b2047fd5389fdd7255cf
ZHJD <2262040671@qq.com>
2020-02-09 15:11:39 +0800

    添加调用thread的函数

--- ch9/kernel/main.c
+++ ch9/kernel/main.c
@@ -1,15 +1,28 @@
 #include "print.h"
 #include "init.h"
 #include "memory.h"
+#include "thread.h"
+
+void func(void* arg);
 
 int main(void) {
     put_str("I am a kernel.\n");
 	init_all();
-    void* vaddr = get_kernel_pages(4);
-    put_str("get kernel pages start virtual address:0x");
-    put_int((uint32_t)vaddr);
-    put_char('\n');
+    
+    thread_start("print", 31, func, "thread_start");
+
    // asm volatile("sti"); // 开中断
 	while(1);
 	return 0;
+}
+
+void func(void* arg)
+{
+    char* ch = (char*)arg;
+    for(int i = 0; i < 10; i++)
+    {
+        put_str(arg);
+        put_char('\n');
+    }
+    while(1);
 }
