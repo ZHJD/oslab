23ceea71dfc2dea48322f2146c48ca824d0b2572
ZHJD <2262040671@qq.com>
2020-02-11 22:18:07 +0800

    使用信号量来解决互斥问题

--- ch10/kernel/main.c
+++ ch10/kernel/main.c
@@ -2,23 +2,28 @@
 #include "init.h"
 #include "memory.h"
 #include "thread.h"
+#include "sync.h"
 
 void func1(void* arg);
 void func2(void* arg);
 
+lock lck;
 
 int main(void) {
     put_str("I am a kernel.\n");
 	init_all();
      
-    thread_start("fun1", 1, func1, "aaa ");
-    thread_start("fun2", 1, func2, "bbb ");
+    thread_start("fun1", 8, func1, "aaa ");
+    thread_start("fun2", 31, func2, "bbb ");
     
     intr_enable();
    // asm volatile("sti"); // 开中断
-	while(1)
+    lock_init(&lck);
+    while(1)
     {
+        lock_acquire(&lck);
         put_str("ccc ");
+        lock_release(&lck);
     };
 	return 0;
 }
@@ -28,8 +33,9 @@ void func1(void* arg)
     char* ch = (char*)arg;
     for(;;)
     {
+        lock_acquire(&lck);
         put_str(arg);
-       // put_char('\n');
+        lock_release(&lck);
     }
     while(1);
 }
@@ -40,8 +46,9 @@ void func2(void* arg)
     char* ch = (char*)arg;
     for(;;)
     {
+        lock_acquire(&lck);
         put_str(arg);
-       // put_char('\n');
+        lock_release(&lck);
     }
     while(1);
 }
