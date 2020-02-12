77030095e7f37c2adb8e4872cc41f65116e2c081
ZHJD <2262040671@qq.com>
2020-02-12 17:55:58 +0800

    注册键盘驱动程序

--- ch10/kernel/init.c
+++ ch10/kernel/init.c
@@ -5,6 +5,7 @@
 #include "memory.h"
 #include "thread.h"
 #include "console.h"
+#include "keyboard.h"
 
 // 负责初始化所有模块
 void init_all(void)
@@ -17,4 +18,5 @@ void init_all(void)
     mem_init();
     thread_init();   
     console_init(); // 控制台初始化
+    keyboard_init();
 }
