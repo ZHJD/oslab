638ee09ced9ec20b04918105e9a289fa1e84da00
ZHJD <2262040671@qq.com>
2020-02-18 14:54:44 +0800

    添加main

--- ch10/kernel/main.c
+++ ch10/kernel/main.c
@@ -5,21 +5,19 @@
 #include "sync.h"
 #include "keyboard.h"
 #include "ioqueue.h"
+#include "string.h"
+
 
 void func1(void* arg);
 void func2(void* arg);
-
-lock lk;
 
 int main(void) {
     put_str("I am a kernel.\n");
 	init_all();
      
-    thread_start("fun1", 2, func1, " 1_ ");
-    thread_start("fun2", 2, func2, " 2_ ");
+    thread_start("fun1", 8, func1, " 1_ ");
+    thread_start("fun2", 31, func2, " 2_ ");
     
-    lock_init(&lk);
-
     intr_enable();
     while(1);
 	return 0;
@@ -27,26 +25,28 @@ int main(void) {
 
 void func1(void* arg)
 {
-    char* str = (char*)arg;
+    char str[20];
+    char* ch = (char*)arg;
+    int len = strlen(ch);
+    strcpy(str, arg);
     while(1)
     {
-        lock_acquire(&lk);
+        str[len] = ioq_getchar(&keyboard_buf);
+        str[len + 1] = '\0';
         console_put_str(str);
-        char byte = ioq_getchar(&keyboard_buf);
-        console_put_char(byte);
-        lock_release(&lk);
     }
 }
 
 void func2(void* arg)
 {
-    char* str = (char*)arg;
+    char str[20];
+    char* ch = (char*)arg;
+    int len = strlen(ch);
+    strcpy(str, arg);
     while(1)
     {
-        lock_acquire(&lk);
+        str[len] = ioq_getchar(&keyboard_buf);
+        str[len + 1] = '\0';
         console_put_str(str);
-        char byte = ioq_getchar(&keyboard_buf);
-        console_put_char(byte);
-        lock_release(&lk);
     }
 }
