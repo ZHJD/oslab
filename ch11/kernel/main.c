69652add626de2e1fc68d33f26741fb0e3829b90
ZHJD <2262040671@qq.com>
2020-02-21 21:51:51 +0800

    测试

--- ch11/kernel/main.c
+++ ch11/kernel/main.c
@@ -6,47 +6,62 @@
 #include "keyboard.h"
 #include "ioqueue.h"
 #include "string.h"
+#include "process.h"
 
 
 void func1(void* arg);
 void func2(void* arg);
+void prog_a();
+void prog_b();
+
+int test_var_a = 0;
+int test_var_b = 0;
 
 int main(void) {
     put_str("I am a kernel.\n");
-	init_all();
+    init_all();
      
-    thread_start("fun1", 8, func1, " 1_ ");
-    thread_start("fun2", 31, func2, " 2_ ");
+//    thread_start("fun1", 31, func1, " 1_ ");
+//    thread_start("fun2", 31, func2, " 2_ ");
+    process_execute(prog_a, "prog_a");
+//    process_execute(prog_b, "prog_b");
     
     intr_enable();
     while(1);
-	return 0;
+    return 0;
 }
 
 void func1(void* arg)
-{
-    char str[20];
-    char* ch = (char*)arg;
-    int len = strlen(ch);
-    strcpy(str, arg);
+{;
     while(1)
     {
-        str[len] = ioq_getchar(&keyboard_buf);
-        str[len + 1] = '\0';
-        console_put_str(str);
+        console_put_str("v_a:0x");
+        console_put_int(test_var_a);
     }
 }
 
 void func2(void* arg)
 {
-    char str[20];
-    char* ch = (char*)arg;
-    int len = strlen(ch);
-    strcpy(str, arg);
     while(1)
     {
-        str[len] = ioq_getchar(&keyboard_buf);
-        str[len + 1] = '\0';
-        console_put_str(str);
+        console_put_str("v_b:0x");
+        console_put_int(test_var_b);
     }
+}
+
+void prog_a()
+{
+    while(1)
+    {
+        test_var_a++;
+    }
+}
+
+void prog_b()
+{
+    while(1)
+    {
+        test_var_b++;
+    }
+
 }
