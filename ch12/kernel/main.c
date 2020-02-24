492bf7fed4398df33acc9acb86a648ca44f0b421
ZHJD <2262040671@qq.com>
2020-02-24 16:48:42 +0800

    测试分配内存和获取进程id，printf函数

--- ch12/kernel/main.c
+++ ch12/kernel/main.c
@@ -7,7 +7,8 @@
 #include "ioqueue.h"
 #include "string.h"
 #include "process.h"
-
+#include "stdio.h"
+#include "syscall.h"
 
 void func1(void* arg);
 void func2(void* arg);
@@ -21,10 +22,10 @@ int main(void) {
     put_str("I am a kernel.\n");
     init_all();
      
-     thread_start("fun1", 31, func1, " 1_ ");
-     thread_start("fun2", 31, func2, " 2_ ");
-      process_execute(prog_a, "prog_a");
-      process_execute(prog_b, "prog_b");
+       thread_start("fun1", 31, func1, " 1_ ");
+       thread_start("fun2", 31, func2, " 2_ ");
+       process_execute(prog_a, "prog_a");
+//       process_execute(prog_b, "prog_b");
     
     intr_enable();
     while(1);
@@ -32,29 +33,26 @@ int main(void) {
 }
 
 void func1(void* arg)
-{;
-    while(1)
-    {
-        console_put_str("v_a:0x");
-        console_put_int(test_var_a);
-    }
+{
+       void* addr =  sys_malloc(33);
+       console_put_str("I am thread_a, sys_malloc(33), addr is 0x");
+       console_put_int((int)addr);
+    console_put_char('\n');
+    while(1);
 }
 
 void func2(void* arg)
 {
-    while(1)
-    {
-        console_put_str("v_b:0x");
-        console_put_int(test_var_b);
-    }
+    void* addr = sys_malloc(33);
+    console_put_str("I am thread_a, sys_malloc(33), addr is 0x");
+    console_put_int((int)addr);
+    console_put_char('\n');
+    while(1);
 }
 
 void prog_a()
 {
-    while(1)
-    {
-        test_var_a++;
-    }
+    printf("pid is %d\n", getpid());
 }
 
 void prog_b()
